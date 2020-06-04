7eea29a22cebf0be0cafeadacc7f0d8c8ffa5c53
Wagner <wagner@rigolidarosa.com>
2020-06-04 20:17:47 -0300

    Dia 4 concluído.

---
+++ src/server.js
@@ -0,0 +1,29 @@
+const express = require("express")
+const server = express()
+
+// configurar pasta pública
+server.use(express.static("public"))
+
+//utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
+// configurar caminhos na minha aplicação
+// página inicial
+server.get("/", (req, res) => {
+    return res.render("index.html", {
+        title: "Seu marketplace de coleta de resíduos"
+    })
+})
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+server.get("/search-results", (req, res) => {
+    return res.render("search-results.html")
+})
+
+//ligar o servidor
+server.listen(3000)
